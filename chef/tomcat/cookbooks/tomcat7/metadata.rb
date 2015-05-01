ea74aa9abb63eb544a8dc0d3ce10a988f55f79ce
root <root@ESCMAPPDB.escm.local>
2015-05-01 19:05:09 +0530

    chef tomcat demo

---
+++ chef/tomcat/cookbooks/tomcat7/metadata.rb
@@ -0,0 +1,7 @@
+name             "tomcat7"
+maintainer       "Vladislav Mikhaylov"
+maintainer_email "solarvm@gmail.com"
+license          "AGPL"
+description      "Installs/Configures tomcat7 binary distrib"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.2"
---
+++ chef/tomcat/solo.rb
@@ -0,0 +1,5 @@
+log_level          :debug
+log_location       STDOUT
+file_cache_path    "/var/demo/chef/tomcat/cache"
+json_attribs       "/var/demo/chef/tomcat/dna.json"
+cookbook_path      "/var/demo/chef/tomcat/cookbooks"
